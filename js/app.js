e945f85b30f93fe9985beb7c337d79c03bcebb77
Romain <phenixstrato@gmail.com>
2019-11-03 21:45:14 +0100

    Gros fix responsiv
    Travail sur la page evenement mais surtout enorme refonte de la partie navigation et header, TOUJOURS UTILISER DES % POUR LES ENFANTS ET NE PAS UTILISER MARGIN POUR ESPACER SUR TOUTE UNE LONGUEUR (thanks aussi la fonction calc() sur les font icon

--- js/app.js
+++ js/app.js
@@ -7,28 +7,54 @@ document.addEventListener('DOMContentLoaded', function () {
     let icon_fleche = document.querySelector('#saison > a > i');
     let section_grid = document.querySelectorAll('.gallery-grid-wrapper');
     let li_collapsed = document.querySelectorAll('.gallery-collapsed-menu>li');
+    //gestion des link navbar
+    let navbar_link = document.querySelectorAll('.navbar-wrapper-link>li>a');
+    let div_link = document.querySelectorAll('.main-bloc');
 
     saison.addEventListener('click', function (event) {
         is_collapsed.classList.toggle('is-active');
         icon_fleche.classList.toggle('fa-caret-right');
         icon_fleche.classList.toggle('fa-sort-down');
         event.preventDefault();
-    })
+    }) //fin saison listener
 
     li_collapsed.forEach(li => {
         li.addEventListener('click', function (event) {
             let target = this.getAttribute('data-target');
             section_grid.forEach(element => {
-                if(element.id == target){
-                    console.log(element.id)
+                if (element.id == target) {
                     element.classList.remove('is-none')
-                }else{
+                } else {
                     element.classList.add('is-none')
                 }
             });
-            
-            
+
+
 
         });
-    })
+    }) //fin li_collapsed for each
+
+    navbar_link.forEach(link => {
+        link.addEventListener('click', function (event) {
+            let target = this.getAttribute('data-target');
+            console.log(target);
+            
+            div_link.forEach(element => {
+                if (element.id == target) {
+                    element.classList.remove('is-none');
+                    event.preventDefault();
+                } else {
+                    element.classList.add('is-none');
+                    event.preventDefault();
+                }
+            });
+
+
+        })
+
+
+
+    }); //fin navbar_link for each
+
+
 }); // fin document ready
