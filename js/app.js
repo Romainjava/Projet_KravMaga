6e9b89e159ee57f78c17be32e2045f84be22a040
Romain <phenixstrato@gmail.com>
2019-11-11 18:45:11 +0100

    ajout de la page tarifs
    page tarifs avec la gestion de l'api leatflet egalement, difficulté à cause du resize dû au display none de toute la section, la map essayé de chargé sur quelque chose qui n'existé pas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     //Charge seulement quand le DOM est entiérement chargé
 
+    //===== Librairie Leatfleat pour la page Tarifs =====//
+    //====== gestion de la resize dû au display none en amonte de la section ici et dans l'event tarifs =====//
+    var map = L.map('map').setView([43.419749, 3.600515], 15);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+    }).addTo(map);
+
+    var marker = L.marker([43.419749, 3.600515]).addTo(map);
+    marker.bindPopup("<b>Dojo Taurus</b><br><span>9 Rue de la Méditerranée</span><br><span> 34140 Mèze.</span>").openPopup();
+    //me permet de resize en dynamic à cause du display none par defaut
+    map.on('resize', function(){  
+        map.invalidateSize(true);
+    })
+
     // ============ Element HTML ============ 
     let saison = document.querySelector('#saison>a');
     let is_collapsed = document.querySelector('.gallery-collapsed-menu');
@@ -16,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let sections_evennement = document.querySelectorAll('.evenement-wrapper');
     let link_evennement = document.querySelectorAll('.evenement-wrapper > .box > .content > a');
     let revenir__link = document.querySelectorAll('.redirection__evenement>.redirection__link');
-
+    let tarif__link = document.querySelector('.topbar-link-right');
 
     // ============ Gere l'affichage de la gallery selon les saisons ============ 
     saison.addEventListener('click', function (event) {
@@ -45,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // ============ Redirige sur chaque section selon le link de la navbar ============ 
     navbar_link.forEach(link => {
         link.addEventListener('click', function (event) {
-            let target = this.getAttribute('data-target');       
+            let target = this.getAttribute('data-target');
             div_link.forEach(element => {
                 if (element.id == target) {
                     element.classList.remove('is-none');
@@ -97,8 +114,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 modal.classList.add('fromRight');
                 active.classList.remove('goesLeft');
                 active.classList.add('is-none');
-                
-                
+
+
             }, 1000);
             event.preventDefault();
         })
@@ -126,8 +143,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }); //fin for each revenir_link
 
+    // ====== Permet à partir de la class main-bloc de recup l'id du bloc et de la comparé à target pour rajouter is-none ou delete ======
+    tarif__link.addEventListener('click', function (event) {
+        let target = this.getAttribute('data-target');
+        div_link.forEach(div => {
+            if (div.id == target) {
+                div.classList.remove('is-none');
+                //me permet de resize en dynamic à cause du display none par defaut sur l'evenement
+                map.invalidateSize(true);
+            } else {
+                div.classList.add('is-none');
+            }
+            event.preventDefault();
+        }); //fin foreach div__link
 
 
+    }) //fin tarif__link
 
+    
 
 }); // fin document ready
